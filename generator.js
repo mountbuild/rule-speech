dc6450db86a6cd36426fc6152bab8b6c2883ae15
Lance Pollard <lp@elk.fm>
2021-07-05 05:21:46 -0700

    add 10000 generated random words

---
+++ generator.js
@@ -0,0 +1,499 @@
+
+const call = require('../call-script')
+const tone = require('../tone-script')
+
+const tones = [
+  ' ',
+  ' +',
+  ' -',
+  '+ ',
+  '- ',
+  '+',
+  '-',
+  '+-',
+  '-+',
+]
+
+const nasals = [
+  '',
+  '~',
+]
+
+const consonants = [
+  '[v+]-yb',
+  '[v+]-yc',
+  '[v+]-yf',
+  '[v+]-yj',
+  '[v+]-yk',
+  '[v+]-yl',
+  '[v+]-ym',
+  '[v+]-yn',
+  '[v+]-yp',
+  '[v+]-ys',
+  '[v+]-yt',
+  '[v+]-yv',
+  '[v+]-yz',
+  '[v+]-st',
+  '[v+]-mp',
+  '[v+]-mpt',
+  '[v+]-mps',
+  '[v+]-nt',
+  '[v+]-lk',
+  '[v+]-st',
+  '[v+]-nst',
+  '[v+]-rd',
+  '[v+]-rg',
+  '[v+]-rk',
+  '[v+]-lf',
+  '[v+]-wl',
+  'bl-[v+]',
+  'fl-[v+]',
+  'pl-[v+]',
+  'gl-[v+]',
+  'kl-[v+]',
+  'b-[v+]',
+  'c-[v+]',
+  'C-[v+]',
+  'd-[v+]',
+  'h-[v+]',
+  'j-[v+]',
+  'dj-[v+]',
+  'k-[v+]',
+  'K-[v+]',
+  'l-[v+]',
+  'L-[v+]',
+  'm-[v+]',
+  'M-[v+]',
+  'n-[v+]',
+  'n-[v+]',
+  'p-[v+]',
+  'r-[v+]',
+  'R-[v+]',
+  's-[v+]',
+  'S-[v+]',
+  't-[v+]',
+  'T-[v+]',
+  'v-[v+]',
+  'V-[v+]',
+  'w-[v+]',
+  'x-[v+]',
+  'X-[v+]',
+  'y-[v+]',
+  'z-[v+]',
+  'Z-[v+]',
+  'q-[v+]',
+
+  '[v+]-bb-[v+]',
+  '[v+]-cc-[v+]',
+  '[v+]-CC-[v+]',
+  '[v+]-dd-[v+]',
+  '[v+]-hh-[v+]',
+  '[v+]-jj-[v+]',
+  '[v+]-kk-[v+]',
+  '[v+]-KK-[v+]',
+  '[v+]-ll-[v+]',
+  '[v+]-LL-[v+]',
+  '[v+]-mm-[v+]',
+  '[v+]-MM-[v+]',
+  '[v+]-nn-[v+]',
+  '[v+]-nn-[v+]',
+  '[v+]-pp-[v+]',
+  '[v+]-rr-[v+]',
+  '[v+]-RR-[v+]',
+  '[v+]-ss-[v+]',
+  '[v+]-SS-[v+]',
+  '[v+]-tt-[v+]',
+  '[v+]-TT-[v+]',
+  '[v+]-vv-[v+]',
+  '[v+]-VV-[v+]',
+  '[v+]-ww-[v+]',
+  '[v+]-xx-[v+]',
+  '[v+]-XX-[v+]',
+  '[v+]-yy-[v+]',
+  '[v+]-zz-[v+]',
+  '[v+]-ZZ-[v+]',
+  '[v+]-qq-[v+]',
+
+  '[v+]-b',
+  '[v+]-q',
+  '[v+]-c',
+  '[v+]-C',
+  '[v+]-d',
+  '[v+]-h',
+  '[v+]-j',
+  '[v+]-dj',
+  '[v+]-dz',
+  '[v+]-k',
+  '[v+]-K',
+  '[v+]-l',
+  '[v+]-L',
+  '[v+]-m',
+  '[v+]-M',
+  '[v+]-n',
+  '[v+]-n',
+  '[v+]-p',
+  '[v+]-r',
+  '[v+]-R',
+  '[v+]-s',
+  '[v+]-S',
+  '[v+]-t',
+  '[v+]-ts',
+  '[v+]-tx',
+  '[v+]-T',
+  '[v+]-TX',
+  '[v+]-v',
+  '[v+]-V',
+  '[v+]-w',
+  '[v+]-x',
+  '[v+]-X',
+  '[v+]-y',
+  '[v+]-z',
+  '[v+]-Z',
+  'by-[v+]',
+  'cy-[v+]',
+  'Cy-[v+]',
+  'dy-[v+]',
+  'hy-[v+]',
+  'jy-[v+]',
+  'djy-[v+]',
+  'dzy-[v+]',
+  'ky-[v+]',
+  'Ky-[v+]',
+  'ly-[v+]',
+  'Ly-[v+]',
+  'my-[v+]',
+  'My-[v+]',
+  'ny-[v+]',
+  'ny-[v+]',
+  'py-[v+]',
+  'ry-[v+]',
+  'Ry-[v+]',
+  'sy-[v+]',
+  'Sy-[v+]',
+  'ty-[v+]',
+  'txy-[v+]',
+  'tsy-[v+]',
+  'Ty-[v+]',
+  'TXy-[v+]',
+  'vy-[v+]',
+  'Vy-[v+]',
+  'wy-[v+]',
+  'xy-[v+]',
+  'Xy-[v+]',
+  'yy-[v+]',
+  'zy-[v+]',
+  'Zy-[v+]',
+  'br-[v+]',
+  'cr-[v+]',
+  'Cr-[v+]',
+  'dr-[v+]',
+  'hr-[v+]',
+  'jr-[v+]',
+  'djr-[v+]',
+  'kr-[v+]',
+  'Kr-[v+]',
+  'lr-[v+]',
+  'Lr-[v+]',
+  'mr-[v+]',
+  'Mr-[v+]',
+  'nr-[v+]',
+  'nr-[v+]',
+  'pr-[v+]',
+  'rr-[v+]',
+  'Rr-[v+]',
+  'sr-[v+]',
+  'Sr-[v+]',
+  'tr-[v+]',
+  'txr-[v+]',
+  'Tr-[v+]',
+  'TXr-[v+]',
+  'vr-[v+]',
+  'Vr-[v+]',
+  'wr-[v+]',
+  'xr-[v+]',
+  'Xr-[v+]',
+  'yr-[v+]',
+  'zr-[v+]',
+  'Zr-[v+]',
+  'bh-[v+]',
+  'ch-[v+]',
+  'Ch-[v+]',
+  'dh-[v+]',
+  'hh-[v+]',
+  'jh-[v+]',
+  'djh-[v+]',
+  'kh-[v+]',
+  'Kh-[v+]',
+  'lh-[v+]',
+  'Lh-[v+]',
+  'mh-[v+]',
+  'Mh-[v+]',
+  'nh-[v+]',
+  'nh-[v+]',
+  'ph-[v+]',
+  'rh-[v+]',
+  'Rh-[v+]',
+  'sh-[v+]',
+  'Sh-[v+]',
+  'th-[v+]',
+  'Th-[v+]',
+  'vh-[v+]',
+  'Vh-[v+]',
+  'wh-[v+]',
+  'xh-[v+]',
+  'Xh-[v+]',
+  'yh-[v+]',
+  'zh-[v+]',
+  'Zh-[v+]',
+  'sk-[v+]',
+  'skr-[v+]',
+  'skl-[v+]',
+  'sn-[v+]',
+  'sm-[v+]',
+  'sl-[v+]',
+  'st-[v+]',
+  '[v+]-zm',
+  '[v+]-sk',
+  '[v+]-tl',
+  '[v+]-dl',
+  '[v+]-sp',
+  'spr-[v+]',
+  'spw-[v+]',
+  'spl-[v+]',
+]
+
+const vowels = [
+  'i',
+  'a',
+  'u',
+  'e',
+  'E',
+  'U',
+  'I',
+  'o',
+  'A',
+  'O',
+  'o#',
+  'u#',
+  'e#',
+  'i#',
+  'a#',
+
+  'ii',
+  'ia',
+  'iu',
+  'ie',
+  'iE',
+  'iU',
+  'iI',
+  'io',
+  'iA',
+  'iO',
+  'io#',
+  'iu#',
+  'ie#',
+  'ii#',
+  'ia#',
+
+  'ai',
+  'aa',
+  'au',
+  'ae',
+  'aE',
+  'aU',
+  'aI',
+  'ao',
+  'aA',
+  'aO',
+  'ao#',
+  'au#',
+  'ae#',
+  'ai#',
+  'aa#',
+
+  'ui',
+  'ua',
+  'uu',
+  'ue',
+  'uE',
+  'uU',
+  'uI',
+  'uo',
+  'uA',
+  'uO',
+  'uo#',
+  'uu#',
+  'ue#',
+  'ui#',
+  'ua#',
+
+  'ei',
+  'ea',
+  'eu',
+  'ee',
+  'eE',
+  'eU',
+  'eI',
+  'eo',
+  'eA',
+  'eO',
+  'eo#',
+  'eu#',
+  'ee#',
+  'ei#',
+  'ea#',
+
+  'oi',
+  'oa',
+  'ou',
+  'oe',
+  'oE',
+  'oU',
+  'oI',
+  'oo',
+  'oA',
+  'oO',
+  'oo#',
+  'ou#',
+  'oe#',
+  'oi#',
+  'oa#',
+]
+
+const randomize = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+
+const generate = ({ size = 3, withNasals = false, withTones = false } = {}) => {
+  const parts = new Array(size)
+  let i = 0
+  while (i < size) {
+    let part = consonants[randomize(0, consonants.length - 1)]
+    parts[i++] = part
+  }
+
+  let string = parts.join('-')
+    .replace(/\[v\+\]\-\[v\+\]/g, () => fetchVowels({ withNasals, withTones }))
+    .replace(/\[v\+\]/g, () => fetchVowels({ withNasals, withTones }))
+    .replace(/\-/g, '')
+    .replace(/bb+/g, 'bb')
+    .replace(/cc+/g, 'cc')
+    .replace(/CC+/g, 'CC')
+    .replace(/dd+/g, 'dd')
+    .replace(/hh+/g, 'hh')
+    .replace(/jj+/g, 'jj')
+    .replace(/kk+/g, 'kk')
+    .replace(/KK+/g, 'KK')
+    .replace(/ll+/g, 'll')
+    .replace(/LL+/g, 'LL')
+    .replace(/mm+/g, 'mm')
+    .replace(/MM+/g, 'MM')
+    .replace(/nn+/g, 'nn')
+    .replace(/nn+/g, 'nn')
+    .replace(/pp+/g, 'pp')
+    .replace(/rr+/g, 'rr')
+    .replace(/RR+/g, 'RR')
+    .replace(/ss+/g, 'ss')
+    .replace(/SS+/g, 'SS')
+    .replace(/tt+/g, 'tt')
+    .replace(/TT+/g, 'TT')
+    .replace(/vv+/g, 'vv')
+    .replace(/VV+/g, 'VV')
+    .replace(/ww+/g, 'ww')
+    .replace(/xx+/g, 'xx')
+    .replace(/XX+/g, 'XX')
+    .replace(/yy+/g, 'yy')
+    .replace(/zz+/g, 'zz')
+    .replace(/ZZ+/g, 'ZZ')
+    .replace(/qq+/g, 'qq')
+
+  return string
+}
+
+const fetchVowels = ({ withNasals = false, withTones = false }) => {
+  const v = vowels[randomize(0, vowels.length - 1)]
+
+  let part
+  let parts = []
+
+  v.split('').forEach(x => {
+    if (x != '#') {
+      part = [x]
+      parts.push(part)
+    } else {
+      part.push(x)
+    }
+  })
+
+  let p = parts.map(x => x.join(''))
+
+  if (withTones) {
+    const t = tones[randomize(0, tones.length - 1)]
+
+    if (t.trim()) {
+      let letter = p.shift()
+
+      t.split('').forEach((x, i) => {
+        const z = `${letter}${x}`.trim()
+        p.splice(i, 0, z)
+      })
+    }
+  }
+
+  if (withNasals) {
+    const n = nasals[randomize(0, nasals.length - 1)]
+
+    if (n) {
+      p = p.map(x => `${x}~`)
+    }
+  }
+
+  return p.join('')
+}
+
+const fs = require('fs')
+const out = [`call-script-encoding`,`call-script-ascii`,`tone-script-encoding`]
+const log = text => {
+  out.push({
+    call: call(text),
+    text: text,
+    tone: tone(text)
+  })
+}
+let i = 0
+while (i++ < 1000) {
+  log(generate({ size: 1 }))
+}
+i = 0
+while (i++ < 1000) {
+  log(generate({ size: 2 }))
+}
+i = 0
+while (i++ < 1000) {
+  log(generate({ size: 2, withTones: true }))
+}
+i = 0
+while (i++ < 1000) {
+  log(generate({ size: 1, withNasals: true }))
+}
+i = 0
+while (i++ < 1000) {
+  log(generate({ size: 1, withNasals: true, withTones: true }))
+}
+i = 0
+while (i++ < 5000) {
+  log(generate({ size: 3 }))
+}
+
+const html = fs.readFileSync('words.template.html', 'utf-8')
+  .replace(/\{WORDS\}/, JSON.stringify(shuffleArray(out), null, 2))
+
+fs.writeFileSync('words.html', html)
+
+function shuffleArray(d) {
+  for (var c = d.length - 1; c > 0; c--) {
+    var b = Math.floor(Math.random() * (c + 1));
+    var a = d[c];
+    d[c] = d[b];
+    d[b] = a;
+  }
+  return d
+};
--- readme.md
+++ readme.md
@@ -21,6 +21,42 @@
 
 Tune speech is a collection of made up sound-sequences (a.k.a. "words"), which don't belong necessarily to any language. Instead they belong to the Earth's languages. It aims to teach you how to pronounce any word in the world. Given that you are able to do that, you can go into Russian, or Hebrew, or Chinese, or Navajo, or Zulu, and speak it like you know what you are talking about.
 
+Try out learning to pronounce [these sound sequences](https://mountbuild.github.io/tune-speech/words) and you'll be off to learning any language with ease.
+
+## Speak Any Language
+
+The first step is to understand the basics of sound production:
+
+1. The vowels.
+1. The nasals.
+1. The lengths.
+1. The stresses.
+1. The consonants.
+1. The geminations.
+1. The ejectives.
+1. The implosives.
+1. The stops.
+1. The tenses.
+1. The aspirations.
+1. The pharyngealizations.
+1. The labializations.
+1. The tones.
+1. The clicks.
+
+Once you can properly distinguish these, both in hearing and speaking, then you can start forming words. There are an enormous number of single-syllable sound combinations, ranging in the thousands if not tens of thousands, let alone multi-syllable words. So it is impractical to practice them all up front. But we can present you with some harder ones up front to give you some initial exposure.
+
+### Vowels
+
+There are 15 vowels, and the English r is one of them.
+
+### Consonants
+
+There are 24 base consonants, and every other consonant is a derivation on these.
+
+### Tones
+
+The trick for tones is to start thinking of it like moving between two octaves at first, to make it clear there are different pitches being used.
+
 <h3 id="mount">Mount</h3>
 
 Tune speech is being developed by the folks at [Mount](https://mount.build), a California-based project for helping humanity master information and computation. Mount started off in the winter of 2008 as a spark of an idea, to forming a company 10 years later in the winter of 2018, to a seed of a project just beginning its development phases. Mount funds tune speech's development. It is entirely bootstrapped by working full time and running [Etsy](https://etsy.com/shop/mountbuild) and [Amazon](https://www.amazon.com/s?rh=p_27%3AMount+Build) shops. Also find us on [Facebook](https://www.facebook.com/mountbuild), [Twitter](https://twitter.com/mountbuild), and [LinkedIn](https://www.linkedin.com/company/mountbuild). Check out our other GitHub projects as well!
